6ead55987a60676852512e78f62f5ff0e05324cd
asifilbal01890 <asifikbal01890@gmail.com>
2023-05-30 11:47:48 +0600

    complate nav responsive

--- src/Components/navbar/Navbar.tsx
+++ src/Components/navbar/Navbar.tsx
@@ -24,15 +24,16 @@ const navbar = () => {
                 </div>
 
                 <div className="navbar-end">
-                    <button className="text-[Inter] font-medium gradient-outline-btn py-[13px] px-[20px] border border-[#FFFFFF4D] rounded-[5px]">Sign in</button>
+                    <button className="text-[Inter] font-medium gradient-outline-btn py-[13px] px-[20px] border border-[#FFFFFF4D] rounded-[5px] hidden lg:flex">Sign in</button>
                     <button className="text-[Inter] font-medium gradient-btn py-[13px] px-[20px] ml-[14px]">Get started free</button>
                 </div>
-                <div className="dropdown">
+                <div className="dropdown dropdown-end">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow rounded-box w-52 items-center border">
                         {menuContent}
+                        <button className="text-[Inter] font-medium gradient-outline-btn py-[13px] px-[20px] border border-[#FFFFFF4D] rounded-[5px]">Sign in</button>
                     </ul>
                 </div>
             </div>
